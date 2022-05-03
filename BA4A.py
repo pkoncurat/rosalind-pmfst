640b438326d085c11ed140e9863e1f851f22a666
pkoncurat <100710583+pkoncurat@users.noreply.github.com>
2022-05-03 15:33:51 +0200

    Add files via upload

---
+++ BA4A.py
@@ -0,0 +1,80 @@
+def ProteinTranslation(RNA,geneticCode):
+    peptide=""
+    for i in range(0,len(RNA),3):
+        tmp=RNA[i:(i+3)]
+        for key,value in geneticCode.items():
+            if tmp==key:
+                peptide+=geneticCode[key]
+    return peptide[:-1]
+
+
+if __name__=="__main__":
+    with open("../Downloads/rosalind_ba4a.txt","r") as f:
+        RNA=f.readline().strip()
+    geneticCode = {
+        "AAA": "K",
+        "AAC": "N",
+        "AAG": "K",
+        "AAU": "N",
+        "ACA": "T",
+        "ACC": "T",
+        "ACG": "T",
+        "ACU": "T",
+        "AGA": "R",
+        "AGC": "S",
+        "AGG": "R",
+        "AGU": "S",
+        "AUA": "I",
+        "AUC": "I",
+        "AUG": "M",
+        "AUU": "I",
+        "CAA": "Q",
+        "CAC": "H",
+        "CAG": "Q",
+        "CAU": "H",
+        "CCA": "P",
+        "CCC": "P",
+        "CCG": "P",
+        "CCU": "P",
+        "CGA": "R",
+        "CGC": "R",
+        "CGG": "R",
+        "CGU": "R",
+        "CUA": "L",
+        "CUC": "L",
+        "CUG": "L",
+        "CUU": "L",
+        "GAA": "E",
+        "GAC": "D",
+        "GAG": "E",
+        "GAU": "D",
+        "GCA": "A",
+        "GCC": "A",
+        "GCG": "A",
+        "GCU": "A",
+        "GGA": "G",
+        "GGC": "G",
+        "GGG": "G",
+        "GGU": "G",
+        "GUA": "V",
+        "GUC": "V",
+        "GUG": "V",
+        "GUU": "V",
+        "UAA": "*",
+        "UAC": "Y",
+        "UAG": "*",
+        "UAU": "Y",
+        "UCA": "S",
+        "UCC": "S",
+        "UCG": "S",
+        "UCU": "S",
+        "UGA": "*",
+        "UGC": "C",
+        "UGG": "W",
+        "UGU": "C",
+        "UUA": "L",
+        "UUC": "F",
+        "UUG": "L",
+        "UUU": "F",
+    }
+    print(ProteinTranslation(RNA,geneticCode))
